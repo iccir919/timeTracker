b43e385f967ad2709441db13f0eca6cb21d3e385
Neil <iccir919@gmail.com>
2020-11-20 07:59:18 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,42 @@
-function accessCalendar() {
-  var calendarName = 'Time Tracker';
-  var calendars = CalendarApp.getCalendarsByName(calendarName);
-  Logger.log('Found %s matching calendars.', calendars.length);
+var calendarName = 'Time Tracker';
+var calendar;
+var todaysEvents;
+var durationMap = {};
+
+function main() {
+  getTodaysEvents();
+  calculateEventsDuration();
+  displayResults();
+}
+
+function getTodaysEvents() {
+  calendar = CalendarApp.getCalendarsByName(calendarName)[0];
+  var today = new Date();
+  todaysEvents = calendar.getEventsForDay(today);
+}
+
+function calculateEventsDuration() {
+  for (let event of todaysEvents) {
+    var title = event.getTitle();
+    var duration = diffMinutes(event.getEndTime(), event.getStartTime());
+    
+    if (durationMap[title]) {
+      durationMap[title] += duration;
+    } else {
+      durationMap[title] = duration;
+    }
+  }
+}
+
+function displayResults() {
+  for (let event in durationMap) {
+    var duration = (durationMap[event] / 60).toFixed(2);
+    Logger.log(`    ${event}: ${duration} hours`);
+  }
+}
+
+function diffMinutes(dt2, dt1) {
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  return Math.abs(Math.round(diff));
 }
