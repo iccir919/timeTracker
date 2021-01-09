b7a4162a95434252185ccd82ef1eee83a62cfc1b
Neil Ricci <iccir919@gmail.com>
2021-01-09 14:24:54 -0800

    Adds Google Calendar data to Google Sheets

--- code.js
+++ code.js
@@ -1,25 +1,39 @@
 var calendarName = 'Time Tracker';
-var fromNumberOfDaysAgo = 50;
+var fromNumberOfDaysAgo = 35;
 
 var calendar = CalendarApp.getCalendarsByName(calendarName)[0];
 
 function main(){
-  var dailyTotalsArray = obtainDailyTotals(fromNumberOfDaysAgo);
+  var dailyTotalsObject = obtainDailyTotals(fromNumberOfDaysAgo);
   
-  logTotals(dailyTotalsArray);
-  visualizeTotals(dailyTotalsArray);
+  logTotals(dailyTotalsObject.dailyTotalsArray);
+  visualizeTotals(dailyTotalsObject);
 }
 
 function logTotals(dailyTotalsArray) {
-  for (let dailyTotal of dailyTotalsArray) {
+  dailyTotalsArray.forEach(function(dailyTotal){
     logResults(dailyTotal);
-  }
+  })
 }
 
-function visualizeTotals(dailyTotalsArray) {
+function visualizeTotals(dailyTotalsObject) {
   var spreadsheet = SpreadsheetApp.openByUrl(
     'https://docs.google.com/spreadsheets/d/1ku1P2ZyD9S7PEs0BubNldl4LDlehlP6x4znfkYhpBoE/edit');
- 
+  
+  var sheet = spreadsheet.getSheets()[0];
+  sheet.appendRow(["date", ...dailyTotalsObject.eventsList])
+  
+  for (var dailyTotals of dailyTotalsObject.dailyTotalsArray) {
+    var row = [formatDate(dailyTotals.date)];
+    
+    for (var event of dailyTotalsObject.eventsList) {
+      if(dailyTotals[event]) row.push(minsToHrs(dailyTotals[event]));
+      else row.push(0)
+    }
+    
+    sheet.appendRow(row);
+  }
+  
 }
 
 function obtainDailyTotals(fromNumberOfDaysAgo) {
@@ -27,17 +41,36 @@ function obtainDailyTotals(fromNumberOfDaysAgo) {
   var indexDate = new Date();
   indexDate.setDate(today.getDate() - fromNumberOfDaysAgo);
   var dailyTotalsArray = [];
+  var eventsList = [];
 
   while (indexDate.getTime() <= today.getTime()) {
-    daysEvents = calendar.getEventsForDay(indexDate);
+    var daysEvents = calendar.getEventsForDay(indexDate);
     
-    dailyTotal = calculateEventsDuration(indexDate, daysEvents);
+    var dailyTotal = {};
+    dailyTotal.date = new Date(indexDate);
+    for (let event of daysEvents) {
+      var title = event.getTitle();
+
+      if(!eventsList.includes(title)) eventsList.push(title);
+
+      var duration = diffMinutes(event.getEndTime(), event.getStartTime());
+      
+      if (dailyTotal[title]) {
+        dailyTotal[title] += duration;
+      } else {
+        dailyTotal[title] = duration;
+      }
+    }
     
     dailyTotalsArray.push(dailyTotal)
     
     indexDate.setDate(indexDate.getDate() + 1);
   }
-  return dailyTotalsArray;
+  
+  return {
+    "dailyTotalsArray": dailyTotalsArray,
+    "eventsList": eventsList
+  };
 }
 
 function calculateEventsDuration(date, events) {
@@ -62,7 +95,7 @@ function logResults(dailyTotal) {
   for (let event in dailyTotal) {
     if (event === "date") continue;
     
-    var duration = (dailyTotal[event] / 60).toFixed(2);
+    var duration = minsToHrs(dailyTotal[event]);
     Logger.log(`      ${event}: ${duration} hours`);
   }
 }
@@ -72,5 +105,12 @@ function diffMinutes(dt2, dt1) {
   diff /= 60;
   return Math.abs(Math.round(diff));
 }
-    
+
+function minsToHrs(minutes) {
+  return (minutes / 60).toFixed(2);
+}
+
+function formatDate(date) {
+  return `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`
+}
     
